988acff4cddd7da26eaac1f73360a11211a7221c
Jie <renjie3@msu.edu>
2022-10-30 10:07:37 -0400

    history_job

--- grandriver_job.sh
+++ grandriver_job.sh
@@ -6,13 +6,13 @@ echo $NEXT_JOB_ID > job_id.log
 MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3 --learn_sigma False"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
 TRAIN_FLAGS="--save_interval 3200 --lr 1e-4 --batch_size 100 --microbatch -1 --class_cond False"
-ADV_FLAGS="--mode adv --output_index True --output_class True --adv_noise_num 5000 --load_model True --model_path /home/renjie3/improved-diffusion/results/30/model064000.pt --adv_step 30 --save_forward_clean_sample False --single_target_image_id 5000 --adv_loss_type negative_forward_bachword_loss --group_model_dir /home/renjie3/improved-diffusion/results/30 --group_model False --group_model_num 20 --random_noise_every_adv_step False"
+ADV_FLAGS="--mode adv --output_index True --output_class True --adv_noise_num 5000 --load_model False --model_path /home/renjie3/improved-diffusion/results/30/model064000.pt --adv_step 30 --save_forward_clean_sample False --single_target_image_id 5000 --adv_loss_type negative_forward_bachword_loss --group_model_dir /home/renjie3/improved-diffusion/results/30 --group_model True --group_model_num 20 --random_noise_every_adv_step True"
 POISON_FLAGS="--poisoned False --poisoned_path /egr/research-dselab/renjie3/renjie/improved-diffusion/results/19/adv_noise"
 
 # model102400.pt ema_0.9999_102400.pt
 
 
-GPU_ID='3'
+GPU_ID='5'
 MY_CMD="mpiexec -n 1 python -u scripts/image_train.py --data_dir /home/renjie3/improved-diffusion/datasets/sub_cifar_train_bird_horse $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $POISON_FLAGS"
 MY_ROOT_PATH=`pwd`
 
