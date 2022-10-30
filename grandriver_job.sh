f979775e64911ee02187b6d0d69f1171e14ff442
Jie Ren <thurenjie@outlook.com>
2022-10-30 23:59:04 +0000

    history_job

--- grandriver_job.sh
+++ grandriver_job.sh
@@ -7,14 +7,14 @@ MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
 TRAIN_FLAGS="--save_interval 6400 --lr 1e-4 --batch_size 100 --microbatch -1 --class_cond False --load_model False --model_path /egr/research-dselab/renjie3/renjie/improved-diffusion/results/10/ema_0.9999_102400.pt"
 ADV_FLAGS="--mode train --output_index True --output_class True --adv_noise_num 5000 --adv_step 30 --save_forward_clean_sample False --single_target_image_id 5000 --adv_loss_type forward_bachword_loss"
-POISON_FLAGS="--poisoned True --poisoned_path /egr/research-dselab/renjie3/renjie/improved-diffusion/results/34/adv_noise"
+POISON_FLAGS="--poisoned True --poisoned_path /home/renjie3/improved-diffusion/results/48/adv_noise"
 
 
 # model102400.pt ema_0.9999_102400.pt
 
 
-GPU_ID='5'
-MY_CMD="mpiexec -n 1 python -u scripts/image_train.py --data_dir /localscratch/renjie/sub_cifar_train_bird_horse $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $POISON_FLAGS"
+GPU_ID='3'
+MY_CMD="mpiexec -n 1 python -u scripts/image_train.py --data_dir /home/renjie3/improved-diffusion/datasets/sub_cifar_train_bird_horse $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $POISON_FLAGS"
 MY_ROOT_PATH=`pwd`
 
 echo "cd ${MY_ROOT_PATH}" > ./cmd/cmd_${JOB_ID}.sh
