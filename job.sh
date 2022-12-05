22182ada681871f55dc11ae77a4229583f52ff69
renjie3 <thurenjie@foxmail.com>
2022-12-05 00:02:58 -0500

    fix adv_gm predict noise bug

--- job.sh
+++ job.sh
@@ -5,24 +5,24 @@ MY_JOB_ROOT_PATH=`pwd`
 cd $MY_JOB_ROOT_PATH
 
 MYTIME="47:59:00"
-MYNTASKS="4"
+MYNTASKS="1"
 MYCPU="5"
-MYGRES="gpu:v100:4"
+MYGRES="gpu:v100:1"
 
 MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3 --learn_sigma False --num_input_channels 3"
 
-DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine --predict_xstart True"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine --predict_xstart False"
 
-TRAIN_FLAGS="--save_interval 5000 --lr 1e-4 --batch_size 100 --source_batch_size 10 --lr_anneal_steps 0 --stop_steps 150000 --microbatch -1 --class_cond False --load_model False --model_path /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/results/91/model000100.pt"
+TRAIN_FLAGS="--save_interval 5000 --lr 1e-4 --batch_size 10 --source_batch_size 10 --lr_anneal_steps 0 --stop_steps 150000 --microbatch -1 --class_cond False --load_model True --model_path /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/results/91/model000100.pt"
 
-ADV_FLAGS="--mode train --poison_mode gradient_matching --output_index True --output_class True --adv_noise_num 9000 --adv_step 30 --save_forward_clean_sample False --single_target_image_id 5000 --adv_loss_type test_t_emb_emb_loss --group_model_dir /egr/research-dselab/renjie3/renjie/improved-diffusion/results/58 --group_model False --group_model_num 6 --random_noise_every_adv_step False --t_seg_num 8 --t_seg_start 0 --t_seg_end 1 --eot_gaussian_num 1"
+ADV_FLAGS="--mode adv --poison_mode gradient_matching --output_index True --output_class True --adv_noise_num 3000 --adv_step 30 --save_forward_clean_sample False --single_target_image_id 5000 --adv_loss_type test_t_emb_emb_loss --group_model_dir /egr/research-dselab/renjie3/renjie/improved-diffusion/results/58 --group_model False --group_model_num 6 --random_noise_every_adv_step False --t_seg_num 8 --t_seg_start 0 --t_seg_end 4 --eot_gaussian_num 2"
 
 GM_FLAGS="--use_dist_adv_sampler True --source_dir /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/datasets/cifar_train_3class_9000_red_bird --source_clean_dir /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/datasets/cifar_train_3class_9000_bird --source_class 0 --one_class_image_num 5000 --optim_mode adam --debug False"
 
-POISON_FLAGS="--poisoned True --poisoned_path /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/results/66106117_1/adv_noise"
+POISON_FLAGS="--poisoned False --poisoned_path /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/results/66106117_1/adv_noise"
 
 JOB_INFO="train sub_cifar"
-MYCOMMEND="mpiexec -n 4 python -u scripts/image_train.py --data_dir /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/datasets/cifar_train_3class_9000 $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $GM_FLAGS $POISON_FLAGS"
+MYCOMMEND="mpiexec -n 1 python -u scripts/image_train.py --data_dir /mnt/home/renjie3/Documents/unlearnable/diffusion/improved-diffusion/datasets/cifar_train_3class_9000_bird $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $GM_FLAGS $POISON_FLAGS"
 
 MYCOMMEND2="python3 test.py -gpu_id 0 -model 1 -attack 1 --pgd_norm 7 -batch_size 50 -path Final/VANILLA_62162198_1/iter_50 --alpha 1000 --num_iter 100 --num_stop 2000 --test_subset --seed 1"
 
