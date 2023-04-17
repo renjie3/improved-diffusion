8464c389b9e92cb87d9dd8d8428831b5e55adc1e
Jie <renjie3@msu.edu>
2023-04-17 16:23:21 -0400

    job_history

--- grandriver_job.sh
+++ grandriver_job.sh
@@ -3,11 +3,11 @@ echo $JOB_ID
 NEXT_JOB_ID=`expr $JOB_ID + 1`
 echo $NEXT_JOB_ID > job_id.log
 
-MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.3 --learn_sigma False"
+MODEL_FLAGS="--image_size 64 --num_channels 128 --num_res_blocks 3 --learn_sigma True"
 
-DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule cosine"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
 
-TRAIN_FLAGS="--save_interval 1000 --lr 1e-4 --batch_size 256 --stop_steps 200000 --microbatch -1 --class_cond False --num_workers 4"
+TRAIN_FLAGS="--save_interval 10000 --lr 1e-4 --batch_size 96 --stop_steps 222222 --microbatch -1 --class_cond True --num_workers 8"
 
 ADV_FLAGS="--mode train --output_index True --output_class True --adv_noise_num 5000 --load_model False --model_path /egr/research-dselab/renjie3/renjie/improved-diffusion/results/10/ema_0.9999_102400.pt --adv_step 30 --save_forward_clean_sample False --single_target_image_id 10002"
 
@@ -16,8 +16,8 @@ POISON_FLAGS="--poisoned False --poisoned_path /egr/research-dselab/renjie3/renj
 # SAMPLE_FLAGS="--batch_size 4 --num_samples 4 --model_path /egr/research-dselab/renjie3/renjie/improved-diffusion/results/66247912_1/ema_0.9999_300000.pt"
 
 
-GPU_ID='2'
-MY_CMD="mpiexec -n 1 python -u scripts/image_train.py --data_dir /localscratch/renjie/cifar_train_gray_normal $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $POISON_FLAGS"
+GPU_ID='0, 1, 2'
+MY_CMD="mpiexec -n 3 python -u scripts/image_train.py --data_dir /localscratch/renjie/stl_freq $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS $ADV_FLAGS $POISON_FLAGS"
 MY_ROOT_PATH=`pwd`
 
 echo "cd ${MY_ROOT_PATH}" > ./cmd/cmd_${JOB_ID}.sh
